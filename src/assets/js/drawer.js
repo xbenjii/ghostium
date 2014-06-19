f1ab5676599094451462c588ec56c4a1b9baa5ff
GitLab <git@ican.codes>
2014-06-19 16:30:47 +0200

    Added posts to sidebar and added a hack to get posts when not on homepage

--- src/assets/js/drawer.js
+++ src/assets/js/drawer.js
@@ -1,14 +1,14 @@
-var Drawer = (function ($, Mzr) {
+var Drawer = (function($, Mzr) {
 
     'use strict';
 
     var transEndEventNames = {
-        'WebkitTransition' : 'webkitTransitionEnd',
-        'MozTransition'    : 'transitionend',
-        'OTransition'      : 'oTransitionEnd otransitionend',
-        'transition'       : 'transitionend'
-      },
-      transEndEventName = transEndEventNames[Mzr.prefixed('transition')];
+        'WebkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'OTransition': 'oTransitionEnd otransitionend',
+        'transition': 'transitionend'
+    },
+        transEndEventName = transEndEventNames[Mzr.prefixed('transition')];
 
     var _body = $('body'),
         _container = $('#container'),
@@ -16,47 +16,60 @@ var Drawer = (function ($, Mzr) {
 
     var module = {
         open: function() {
-          _body.addClass('drawer-open drawer-transition');
+            _body.addClass('drawer-open drawer-transition');
+            $.get('/ghost/api/v0.1/posts/')
+                .sucess(function(data) {
+                    $('.drawer-posts').empty(function() {
+                        data.posts.forEach(function(post) {
+                            $('.drawer-posts').append(
+                                ['<li class="drawer-list-item">',
+                                    '<a href="' + post.slug + '" itemprop="url" data-pjax title="' + post.title + '"><i class="fa fa-book"></i>' + post.title + '</a>',
+                                    '</li>'
+                                ].join("")
+                            );
+                        })
+                    })
+                });
         },
         close: function() {
-          _body.removeClass('drawer-open');
+            _body.removeClass('drawer-open');
         },
         isOpen: function() {
-          return _body.hasClass('drawer-open');
+            return _body.hasClass('drawer-open');
         },
         bindOverlay: function() {
-          _overlay.on('click', function(e) {
-            e.preventDefault();
+            _overlay.on('click', function(e) {
+                e.preventDefault();
 
-            if(_body.hasClass('drawer-open')) {
-              module.close();
-            }
-          });
+                if (_body.hasClass('drawer-open')) {
+                    module.close();
+                }
+            });
         },
         bindAnchors: function() {
-          $('.drawer-list a').on('click', function(e) {
-            module.close();
-          });
+            $('.drawer-list a').on('click', function(e) {
+                module.close();
+            });
         },
         bindContainerTransEnd: function() {
-          _container.on(transEndEventName, function(e) {
-            if(!_body.hasClass('drawer-open')) {
-              _body.removeClass('drawer-transition');
-            }
-          });
+            _container.on(transEndEventName, function(e) {
+                if (!_body.hasClass('drawer-open')) {
+                    _body.removeClass('drawer-transition');
+                }
+            });
         },
         init: function() {
-          module.bindOverlay();
-          module.bindAnchors();
-          module.bindContainerTransEnd();
+            module.bindOverlay();
+            module.bindAnchors();
+            module.bindContainerTransEnd();
         }
     };
 
     return {
-      init: module.init,
-      open: module.open,
-      close: module.close,
-      isOpen: module.isOpen
+        init: module.init,
+        open: module.open,
+        close: module.close,
+        isOpen: module.isOpen
     };
 
 }(jQuery, Modernizr));
